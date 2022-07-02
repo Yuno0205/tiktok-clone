aeec8e8e5dfd5419f05fe9b23d14af3d78ca0041
Yuno <mainhathao195@gmail.com>
2022-07-02 15:22:34 +0700

    Build logo and search fill

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,15 +1,36 @@
 import classNames from "classnames/bind";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark , faMagnifyingGlass, faSpinner} from "@fortawesome/free-solid-svg-icons";
+
 import styles from "./Header.module.scss";
+import images from "~/assets/images";
 
 const cx = classNames.bind(styles);
 
 function Header() {
-  return <header className={cx('wrapper')}>
-     <div className={cx('inner')}>
-        {/* Logo */}
-     </div>
-  </header>
+  return (
+    <header className={cx("wrapper")}>
+      <div className={cx("inner")}>
+        <div className={cx("logo")}>
+             <img src={images.logo} alt="Tiktok"/>
+        </div>
+        <div className={cx("search")}>
+            <input placeholder="Search accounts and videos" spellCheck={false}/>
+            <button>
+              <FontAwesomeIcon className={cx("clear")} icon={faCircleXmark}/>
+            </button>
+            <FontAwesomeIcon className={cx("loading")} icon={faSpinner}/>
 
+            <button className={cx("search-btn")}>
+              <FontAwesomeIcon icon={faMagnifyingGlass}/>
+            </button>
+        </div>
+        <div className={cx("action")}>
+
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;

add72a81b1da7fbc365f9aff92604becb22c0024
Yuno <mainhathao195@gmail.com>
2022-07-04 04:42:39 +0700

    Custom Change Language

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -36,7 +36,21 @@ function Header() {
     {
       icon : <FontAwesomeIcon icon={faEarthAsia}/>,
       title : "English",
-      
+      children : {
+        title : "Language" ,
+        data : [
+          {
+            type : "language",
+            code : "en",
+            title : "English",
+          },
+          {
+            type : "language",
+            code : "vi",
+            title : "Tiếng Việt",
+          },
+        ],
+      }
 
     },
     {
@@ -51,6 +65,17 @@ function Header() {
       
     }
   ];
+
+  const handleMenuChange = (menuItem) => {
+      switch (menuItem.type) {
+        case "language":
+          //Handle change language
+          break;
+      
+        default:
+          break;
+      }
+  }
   return (
     <header className={cx("wrapper")}>
       <div className={cx("inner")}>
@@ -88,7 +113,7 @@ function Header() {
           <Button text>Upload</Button>
           <Button primary>Log in</Button>
 
-          <Menu items = {MENU_ITEMS}>
+          <Menu items = {MENU_ITEMS} onChange={handleMenuChange}>
             <button className={cx("more-btn")}>
               <FontAwesomeIcon icon={faEllipsisVertical} />
             </button>
